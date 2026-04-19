2599beeafaa8310d08fcfeb6a49f3daa219a8d5b
Aftab Alam <course.aftabalam@gmail.com>
2026-04-19 22:11:47 +0530

    Second largest, rotate array  and some basic concept of arrays

--- 02-questions.js
+++ 02-questions.js
@@ -13,7 +13,7 @@ function secondLargest(arr) {
   }
   return sortedArr[1];
 }
-console.log(secondLargest([0]));
+// console.log(secondLargest([0]));
 
 // ? Optimized way
 function secondLargestOptimized(arr) {
@@ -30,4 +30,36 @@ function secondLargestOptimized(arr) {
   }
   return secondLargest;
 }
-console.log(secondLargestOptimized(arr));
+// console.log(secondLargestOptimized(arr));
+
+
+/* 
+! Rotate Array by K
+! Given an integer array nums, rotate the array to the by k steps, where k is non - negative
+*/
+
+/*
+* Whave to shift the last elem into the first 
+* Input: nums = [1,2,3,4,5,6,7], k=3 --->> output: [5,6,7,1,2,3]
+
+? we have to do is 
+* [1,2,3,4,5,6,7] => [7,1,2,3,4,5,6] => [6,7,1,2,3,4,5] => [5,6,7,1,2,3,4]
+*/
+
+function rotateArray(arr,k){
+    let size = arr.length; 
+    if(k>size) return "rotate number is too large";
+
+    // let rotateArr=arr;
+
+    for(let i =1; i<= k; i++){
+        const lastNum = arr.pop();
+    // console.log(lastNum);
+    arr.unshift(lastNum)
+    // console.log(arr);
+    }
+    
+    return arr;
+}
+const arr3 = [1,2,3,4,5,6,7]
+console.log(rotateArray(arr3,9));
